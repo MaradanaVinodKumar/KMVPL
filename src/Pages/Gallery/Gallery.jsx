88aeef5a3f8b0f3a660cc8c60ac7ccc019a3250d
vinodkumarmaradana2514@gmail.com <vinodkumarmaradana2514@gmail.com>
2024-02-14 09:44:49 +0530

    Latest

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -23,6 +23,8 @@ const Gallery = () => {
   const [getFormatedDates, setFormatedDates] = useState([
     `${selectedYear}-${selectedMonth}`,
   ]);
+
+  const [MaxDate] = useState({ year: 2023, month: 12 })
   const [MonthsCount, setMonthsCount] = useState(0);
   const [showMonths, setShowMonths] = useState(false); // To toggle display of months
 
@@ -36,6 +38,7 @@ const Gallery = () => {
 
   const handleYearChange = (e) => {
     setSelectedYear(e.target.value);
+    setFormatedDates([`${e.target.value}-${selectedMonth}`]);
     setMonthsCount(0);
     setShowMonths(true); // Show months when year changes
   };
@@ -51,18 +54,19 @@ const Gallery = () => {
       <Container fluid>
         <Row>
           <Col md={3} className="mb-3">
-            <Col className="datePicker">
-             
+            <Col className="datePicker" style={{ border: 1, borderStyle: "solid", borderColor: '#e9e4c9' }}>
+
               <br />
               <select
                 value={selectedYear}
                 onChange={handleYearChange}
                 className="select"
+                style={{ border: 1, borderStyle: "solid" }}
               >
-                {Array.from({ length: 10 }, (_, i) => year - i).map((yearValue) => (
-                  <option 
-                    key={yearValue} 
-                    value={yearValue} 
+                {Array.from({ length: (year + 1) - MaxDate.year }, (_, i) => year - i).map((yearValue) => (
+                  <option
+                    key={yearValue}
+                    value={yearValue}
                     className={yearValue === parseInt(selectedYear) ? 'active' : ''}
                   >
                     {yearValue}
@@ -70,32 +74,33 @@ const Gallery = () => {
                 ))}
               </select>
               <br />
-              {showMonths && (
-                <div
-                  className="scrollable-months"
-                  style={{ maxHeight: "150px", overflowY: "auto" }}
-                >
-                  {/* Display all twelve months */}
-                  <Row>
-                    {Array.from({ length: 12 }, (_, i) => i + 1).map(
-                      (month) => (
-                        <Col key={month} xs={12}>
-                          <button
-                            className={`btn btn-light w-100 mb-2 months_font ${parseInt(selectedMonth) === month ? 'active' : ''}`}
-                            value={month.toString().padStart(2, "0")}
-                            onClick={handleMonthChange}
-                          >
-                            {new Date(selectedYear, month - 1).toLocaleString(
-                              "default",
-                              { month: "long" }
-                            )}
-                          </button>
-                        </Col>
-                      )
-                    )}
-                  </Row>
-                </div>
-              )}
+              {
+                (
+                  <div
+                    className="scrollable-months"
+                    style={{ maxHeight: "150px", overflowY: "auto", }}
+                  >
+                    {/* Display all twelve months */}
+                    <Row>
+                      {Array.from({ length: 12 }, (_, i) => i + 1).map(
+                        (month) => (
+                          <Col key={month} xs={12}>
+                            <button
+                              className={`btn btn-light w-100 mb-2 months_font ${parseInt(selectedMonth) === month ? 'active' : ''}`}
+                              value={month.toString().padStart(2, "0")}
+                              onClick={handleMonthChange}
+                            >
+                              {new Date(selectedYear, month - 1).toLocaleString(
+                                "default",
+                                { month: "long" }
+                              )}
+                            </button>
+                          </Col>
+                        )
+                      )}
+                    </Row>
+                  </div>
+                )}
             </Col>
           </Col>
           <Col md={9} className="GalleryCol">
